5626ab06c0e6a9e1897bd37262fc99a0b33b57c6
almaaaddara <almaditha22@gmail.com>
2023-09-27 22:22:29 +0800

    update ui login, halaman profil user

--- lib/presentation/owner/loginPageOwner.dart
+++ lib/presentation/owner/loginPageOwner.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:kostion/data/model/userProfile.dart';
-// import 'package:kostion/presentation/admin/adminHomePage.dart';
 import 'package:kostion/presentation/owner/ownerHomePage.dart';
 import 'package:kostion/presentation/registrationPage.dart';
 import 'package:kostion/presentation/resetpasswordpage.dart';
-// import 'package:kostion/presentation/tenant/tenantHomePage.dart';
 
 class LoginPageOwner extends StatefulWidget {
   @override
@@ -16,130 +14,115 @@ class _LoginPageOwnerState extends State<LoginPageOwner> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('KostIon'),
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                // Column(
-                //   children: <Widget>[
-                //     RadioListTile<UserType>(
-                //       title: const Text('Penghuni Kost'),
-                //       value: UserType.tenant,
-                //       groupValue: selectedUserType,
-                //       onChanged: (UserType? value) {
-                //         setState(() {
-                //           selectedUserType = value;
-                //         });
-                //       },
-                //     ),
-                //     RadioListTile<UserType>(
-                //       title: const Text('Pemilik Kost'),
-                //       value: UserType.owner,
-                //       groupValue: selectedUserType,
-                //       onChanged: (UserType? value) {
-                //         setState(() {
-                //           selectedUserType = value;
-                //         });
-                //       },
-                //     ),
-                //   ],
-                // ),
-                const SizedBox(height: 20.0),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: 'Username',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(18),
-                          borderSide: BorderSide.none),
-                      fillColor:
-                          Theme.of(context).primaryColor.withOpacity(0.1),
-                      filled: true,
-                      prefixIcon: Icon(Icons.person)),
-                ),
-                const SizedBox(height: 20.0),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      labelText: 'Password',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(18),
-                          borderSide: BorderSide.none),
-                      fillColor:
-                          Theme.of(context).primaryColor.withOpacity(0.1),
-                      filled: true,
-                      prefixIcon: Icon(Icons.lock)),
-                ),
-                const SizedBox(height: 20.0),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => OwnerHomePage()),
-                    );
-                    // Tambahkan logika autentikasi di sini
-                    // Jika autentikasi berhasil, arahkan pengguna ke halaman beranda sesuai tipe pengguna
-                    // if (selectedUserType == UserType.tenant) {
-                    //   Navigator.pushReplacement(
-                    //     context,
-                    //     MaterialPageRoute(
-                    //         builder: (context) => TenantHomePage()),
-                    //   );
-                    // } else if (selectedUserType == UserType.owner) {
-                    //   Navigator.pushReplacement(
-                    //     context,
-                    //     MaterialPageRoute(
-                    //         builder: (context) => OwnerHomePage()),
-                    //   );
-                    // } else if (selectedUserType == UserType.admin) {
-                    //   Navigator.pushReplacement(
-                    //     context,
-                    //     MaterialPageRoute(
-                    //         builder: (context) => AdminHomePage()),
-                    //   );
-                    // }
-                  },
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    shape: StadiumBorder(),
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => RegistrationPage()),
-                    );
-                  },
-                  child: const Text('Daftar'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ResetPasswordPage()),
-                    );
-                  },
-                  child: const Text('Lupa Password?'),
-                ),
-              ],
-            ),
-          ),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('KostIon'),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(24),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _header(context),
+                _inputfield(context),
+                _forgotpassword(context),
+                _registrasi(context),
+              ]),
         ),
       ),
+    );
+  }
+
+  _header(context) {
+    return Column(
+      children: [
+        Text(
+          'Selamat Datang Pemilik Kos!',
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.amber),
+        ),
+        Text(
+          'Silahkan Login',
+          style: TextStyle(fontSize: 20, color: Colors.amber),
+        ),
+      ],
+    );
+  }
+
+  _inputfield(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          decoration: InputDecoration(
+              labelText: "Username",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(18),
+                  borderSide: BorderSide.none),
+              fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+              filled: true,
+              prefixIcon: Icon(Icons.person)),
+        ),
+        SizedBox(height: 15),
+        TextField(
+          decoration: InputDecoration(
+              labelText: "Password",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(18),
+                  borderSide: BorderSide.none),
+              fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+              filled: true,
+              prefixIcon: Icon(Icons.lock)),
+          obscureText: true,
+        ),
+        SizedBox(height: 15),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => OwnerHomePage()),
+            );
+          },
+          child: Text(
+            'Login',
+            style: TextStyle(fontSize: 20),
+          ),
+          style: ElevatedButton.styleFrom(
+            shape: StadiumBorder(),
+            padding: EdgeInsets.symmetric(vertical: 16),
+          ),
+        )
+      ],
+    );
+  }
+
+  _forgotpassword(context) {
+    return TextButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ResetPasswordPage()),
+        );
+      },
+      child: const Text('Lupa Password?'),
+    );
+  }
+
+  _registrasi(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Belum memiliki akun?'),
+        TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => RegistrationPage()),
+              );
+            },
+            child: const Text('Daftar'))
+      ],
     );
   }
 }
--- lib/presentation/tenant/userProfil.dart
+++ lib/presentation/tenant/userProfil.dart
@@ -3,42 +3,82 @@ import 'package:flutter/material.dart';
 class TenantProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Profil Pengguna'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Profil Pengguna'),
+          backgroundColor: Colors.amber,
+        ),
+        body: UserProfile(),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 20.0),
-          CircleAvatar(
-            radius: 70.0,
-            backgroundImage: AssetImage(
-                'assets/user_avatar.jpg'), // Ganti dengan gambar profil pengguna
-          ),
-          SizedBox(height: 10.0),
-          Text(
-            'Nama Pengguna',
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
+    );
+  }
+}
+
+class UserProfile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 80.0,
+                    backgroundImage: AssetImage(
+                        'assets/user_avatar.jpg'), // Ganti dengan gambar profil pengguna
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'John Doe',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color:
+                          Colors.black, // Ganti dengan warna teks yang sesuai
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'Email: user@example.com', // Ganti dengan informasi profil pengguna
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color:
+                          Colors.black, // Ganti dengan warna teks yang sesuai
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-          SizedBox(height: 10.0),
-          Text(
-            'Email: pengguna@example.com', // Ganti dengan alamat email pengguna
-            style: TextStyle(
-              fontSize: 16.0,
-              color: Colors.grey,
-            ),
+          SizedBox(height: 16.0),
+          ListTile(
+            leading: Icon(Icons.person),
+            title:
+                Text('Tentang Saya'), // Ganti dengan informasi profil pengguna
           ),
-          SizedBox(height: 20.0),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.phone),
+            title: Text('0878xxxxx'), // Ganti dengan informasi profil pengguna
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.location_on),
+            title: Text(
+                'Jl.Sbhgxyennsjhsd No 1xx'), // Ganti dengan informasi profil pengguna
+          ),
+          Divider(),
+          // Tambahkan informasi profil lainnya di sini sesuai kebutuhan Anda
           ElevatedButton(
             onPressed: () {
-              // Tambahkan logika untuk mengarahkan pengguna ke halaman edit profil
+              // Tambahkan logika untuk mengganti akun di sini, arahkan ke formulir ubah profil
             },
-            child: Text('Edit Profil'),
-          ),
+            child: Text('Ubah Profil'),
+          )
         ],
       ),
     );
