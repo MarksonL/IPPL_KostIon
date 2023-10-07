c4f1e59927d8be68ad9480474c8d469f64db6942
almaaaddara <134265885+almaaaddara@users.noreply.github.com>
2023-10-08 00:00:17 +0800

    animas loading

--- lib/screen/auth.dart
+++ lib/screen/auth.dart
@@ -2,9 +2,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:kostlon/screen/member/home_member.dart';
 import 'package:kostlon/screen/owner/home_owner.dart';
 import 'package:kostlon/utils/color_theme.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -20,14 +22,17 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _email = TextEditingController();
   final TextEditingController _password = TextEditingController();
 
-  void submit(context) async {
+  void submit(BuildContext context) async {
+    context.loaderOverlay.show();
     try {
       final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: _email.text.trim(),
         password: _password.text.trim(),
       );
       checkRole(context);
+      context.loaderOverlay.hide();
     } on FirebaseAuthException catch (e) {
+      context.loaderOverlay.hide();
       showDialog(
           context: context,
           builder: (context) {
@@ -64,54 +69,65 @@ class _LoginPageState extends State<LoginPage> {
       checkRole(context);
     }
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Text(
-                'KostIon',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            SizedBox(height: 30),
-            TextInput(
-              val: _email,
-              label: 'Email',
-              isPassword: false,
-            ),
-            SizedBox(height: 10),
-            TextInput(
-              val: _password,
-              isPassword: true,
-              label: 'Password',
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () => submit(context),
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  elevation: 0,
-                  backgroundColor: AppColor.primary,
-                  padding: const EdgeInsets.symmetric(vertical: 20),
-                ),
+      body: LoaderOverlay(
+        useDefaultLoading: false,
+        overlayColor: Colors.black12,
+        overlayWidget: Center(
+          child: SpinKitCircle(
+            color: Colors.black,
+            size: 50.0,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
                 child: Text(
-                  'Masuk',
+                  'KostIon',
                   style: TextStyle(
-                    color: Colors.white,
+                    fontSize: 30,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-            )
-          ],
+              SizedBox(height: 30),
+              TextInput(
+                val: _email,
+                label: 'Email',
+                isPassword: false,
+              ),
+              SizedBox(height: 10),
+              TextInput(
+                val: _password,
+                isPassword: true,
+                label: 'Password',
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () => submit(context),
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    elevation: 0,
+                    backgroundColor: AppColor.primary,
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                  ),
+                  child: Text(
+                    'Masuk',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
