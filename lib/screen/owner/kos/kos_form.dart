16c25bd719b118686d6b4d4d9512519d6c442b73
almaaaddara <134265885+almaaaddara@users.noreply.github.com>
2023-10-21 22:42:32 +0800

    Menambahkan fungsi upload file ke firebase sotrage

--- lib/screen/owner/kos/kos_form.dart
+++ lib/screen/owner/kos/kos_form.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: prefer_const_constructors, use_build_context_synchronously, sort_child_properties_last
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
@@ -14,6 +17,8 @@ class OwnerKostFormPage extends StatefulWidget {
 }
 
 class _OwnerKostFormPageState extends State<OwnerKostFormPage> {
+  final storageRef = FirebaseStorage.instance.ref();
+
   final TextEditingController _namaController = TextEditingController();
   final TextEditingController _alamatController = TextEditingController();
   final TextEditingController _hargaController = TextEditingController();
@@ -31,10 +36,26 @@ class _OwnerKostFormPageState extends State<OwnerKostFormPage> {
   }
 
   void submit(BuildContext context) async {
+    if (_selectedImage != null) {
+      final fileRef = storageRef.child('kos/${_selectedImage!.name}');
+      final File file = File(_selectedImage!.path);
+      try {
+        final resp = await fileRef.putFile(file);
+        final url = await fileRef.getDownloadURL();
+        storeData(context, url.toString());
+        debugPrint(url.toString());
+      } catch (e) {
+        debugPrint('terjadi kesalahan');
+      }
+    }
+  }
+
+  void storeData(BuildContext context, String imgPath) async {
     // ambil data user yang sedang login
     User? owner = FirebaseAuth.instance.currentUser;
     //
     Map<String, dynamic> body = {
+      "image": imgPath,
       "name": _namaController.text,
       "owner": owner?.displayName,
       "owner_id": owner?.uid,
@@ -50,9 +71,8 @@ class _OwnerKostFormPageState extends State<OwnerKostFormPage> {
       reset();
       // navigasi ke halaman utama
       Navigator.pop(context);
-      print('sucess');
     } catch (e) {
-      print(e);
+      debugPrint(e.toString());
     }
   }
 
@@ -78,6 +98,7 @@ class _OwnerKostFormPageState extends State<OwnerKostFormPage> {
       appBar: AppBar(
         title: Text("Formulir tambah kos"),
         centerTitle: true,
+        elevation: 0,
       ),
       body: SafeArea(
         child: ListView(
@@ -100,61 +121,107 @@ class _OwnerKostFormPageState extends State<OwnerKostFormPage> {
                       fit: BoxFit.cover,
                     ),
             ),
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(horizontal: 20),
+            //   child: ElevatedButton(
+            //     onPressed: () => _uploadFile(),
+            //     child: Text('Upload File'),
+            //   ),
+            // ),
             SizedBox(height: 20),
-            TextField(
-              controller: _namaController,
-              decoration: InputDecoration(labelText: 'Nama Kos'),
+            TextInput(
+              inputController: _namaController,
+              label: 'Nama Kos',
+              type: TextInputType.text,
             ),
             SizedBox(height: 20),
-            TextField(
-              controller: _alamatController,
-              decoration: InputDecoration(labelText: 'Alamat'),
+            TextInput(
+              inputController: _alamatController,
+              label: 'Alamat',
+              type: TextInputType.text,
             ),
             SizedBox(height: 20),
-            TextField(
-              controller: _hargaController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Harga (per bulan)'),
+            TextInput(
+              inputController: _hargaController,
+              label: 'Harga (per bulan)',
+              type: TextInputType.number,
             ),
             SizedBox(height: 20),
-            Text(
-              'Jumlah Kamar',
-              style: TextStyle(fontSize: 15),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.remove),
-                  onPressed: () {
-                    setState(() {
-                      if (_jumlahKamar > 1) {
-                        _jumlahKamar--;
-                      }
-                    });
-                  },
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                'Jumlah Kamar',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black54,
                 ),
-                Text(
-                  '$_jumlahKamar',
-                  style: TextStyle(fontSize: 18),
-                ),
-                IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: () {
-                    setState(() {
-                      _jumlahKamar++;
-                    });
-                  },
-                ),
-              ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.remove),
+                    onPressed: () {
+                      setState(() {
+                        if (_jumlahKamar > 1) {
+                          _jumlahKamar--;
+                        }
+                      });
+                    },
+                  ),
+                  Text(
+                    '$_jumlahKamar',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      setState(() {
+                        _jumlahKamar++;
+                      });
+                    },
+                  ),
+                ],
+              ),
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => submit(context),
-              child: Text('Simpan'),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: ElevatedButton(
+                onPressed: () => submit(context),
+                child: Text('Simpan'),
+              ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class TextInput extends StatelessWidget {
+  const TextInput(
+      {super.key,
+      required this.label,
+      required TextEditingController inputController,
+      required this.type})
+      : _controller = inputController;
+
+  final TextEditingController _controller;
+  final String label;
+  final TextInputType type;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: TextField(
+        controller: _controller,
+        keyboardType: type,
+        decoration: InputDecoration(labelText: label),
       ),
     );
   }
