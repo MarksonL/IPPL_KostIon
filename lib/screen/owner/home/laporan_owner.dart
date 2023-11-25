af51932d04543e04aa3bc90db424dd9cdcc88299
almaaaddara <almaditha22@gmail.com>
2023-11-26 02:51:49 +0800

    Update laporan_owner.dart

--- lib/screen/owner/home/laporan_owner.dart
+++ lib/screen/owner/home/laporan_owner.dart
@@ -37,12 +37,11 @@ class _LaporanOwnerScreenState extends State<LaporanOwnerScreen> {
             return ListTile(
               title: Text("${item['nama_kos']} - Kamar (${item['no_kamar']})"),
               subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Laporan: ${item['kerusakan']}"),
-                  Text("Status Laporan: ${item['status']}"),
-                ],
-              ),
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Laporan: ${item['kerusakan']}"),
+                    Text("Deskripsi: ${item['deskripsi']}"),
+                  ]),
               onTap: () {
                 final String id = laporan[index].id;
                 Navigator.push(
